b47b67e9dd7214ff21d4287e0aa0fc7e7d706f25
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-08 00:34:38 +0300

    modified:   portfolio/core/__pycache__/views.cpython-310.pyc modified:   portfolio/core/views.py

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from .models import Home
+
 def index(request):
-    return render(request, 'core/index.html')
+    background_image = Home.objects.get()
+    name = Home.objects.get()
+    official_image = Home.objects.get()
+
+    return render(request, 'core/index.html', {
+        'background_image': background_image,
+        'name': name,
+        'official_image': official_image,
+    })

3042d1e847edddd607e67e40817a590e8d287bd5
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-07 23:42:36 +0300

    modified:   portfolio/core/__pycache__/admin.cpython-310.pyc modified:   portfolio/core/__pycache__/models.cpython-310.pyc modified:   portfolio/core/admin.py modified:   portfolio/core/models.py modified:   portfolio/db.sqlite3

--- portfolio/core/models.py
+++ portfolio/core/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Home(models.Model):
     background_image = models.ImageField(upload_to='home_images', blank=True, null=True)
     name = models.CharField(max_length=255)
-    
+    official_image = models.ImageField(uppload_to='home_images', blank=True, null=True)

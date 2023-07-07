b9036fa4e0adc4b605630675c55a491caa2d0045
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-07 19:29:38 +0300

    modified:   portfolio/core/templates/core/base.html modified:   portfolio/core/templates/core/index.html modified:   portfolio/static/css/style.css modified:   portfolio/static/js/javascript.js

--- portfolio/static/js/javascript.js
+++ portfolio/static/js/javascript.js
@@ -1,13 +1,42 @@
-$('.navTrigger').click(function () {
+
+
+/**
+ * 
+$(document).ready(function() {
+    $('.navTrigger').click(function() {
+      $(this).toggleClass('active');
+      $("#mainListDiv").toggleClass("show_list");
+    });
+  
+    $('.navlinks li a').click(function() {
+      $('.navTrigger').removeClass('active');
+      $("#mainListDiv").removeClass("show_list");
+    });
+  
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+      } else {
+        $('.nav').removeClass('affix');
+      }
+    });
+  });
+  
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * References
+ * $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 
 });
-
-/**
- * References
  * 
  * https://codepen.io/ianmalobamwakha/pen/xxQXVyR
  * 

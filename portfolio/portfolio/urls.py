0463d6076a724579c1b051d483d8bc0ee24b3a65
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-09 05:51:46 +0300

    modified:   portfolio/core/__pycache__/views.cpython-310.pyc modified:   portfolio/core/templates/core/base.html modified:   portfolio/core/templates/core/index.html modified:   portfolio/core/views.py modified:   portfolio/db.sqlite3 modified:   portfolio/portfolio/__pycache__/settings.cpython-310.pyc modified:   portfolio/portfolio/__pycache__/urls.cpython-310.pyc modified:   portfolio/portfolio/urls.py modified:   portfolio/static/css/style.css

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,18 +16,11 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib import admin
 from django.urls import path
-
 from core.views import index
+from django.contrib import admin
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# Serving the media files in development mode
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += staticfiles_urlpatterns()

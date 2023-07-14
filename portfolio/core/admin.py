c74d4b625d4dd7ee3a9cca689915a6be932cdae8
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-14 21:40:36 +0300

    modified:   portfolio/core/__pycache__/models.cpython-310.pyc modified:   portfolio/core/admin.py new file:   portfolio/core/migrations/0004_skill.py new file:   portfolio/core/migrations/__pycache__/0004_skill.cpython-310.pyc modified:   portfolio/core/models.py modified:   portfolio/db.sqlite3

--- portfolio/core/admin.py
+++ portfolio/core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Home, About
+from .models import Home, About, Skill
 
 admin.site.register(Home)
 
 admin.site.register(About)
+
+admin.site.register(Skill)
--- portfolio/core/models.py
+++ portfolio/core/models.py
@@ -13,3 +13,19 @@ class About(models.Model):
 
     def __str__(self):
         return self.unofficial_image
+
+
+
+from django.db import models
+
+class Skill(models.Model):
+    SKILL_TYPE_CHOICES = (
+        ('professional', 'Professional'),
+        ('personal', 'Personal'),
+    )
+
+    name = models.CharField(max_length=100)
+    skill_type = models.CharField(max_length=20, choices=SKILL_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name

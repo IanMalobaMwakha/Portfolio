4270faf20eb9a7f803ae2a051dfa0f2dd13cb7c0
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-07-14 21:43:54 +0300

    modified:   portfolio/core/admin.py modified:   portfolio/core/views.py

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 
-from .models import Home, About
+from .models import Home, About, Skill
 
 def index(request):
     background_image = Home.objects.get()
@@ -22,3 +22,15 @@ def about(request):
     return render(request, 'core/index.html', {
         'unofficial_image': unofficial_image,
     })
+
+
+
+def skills(request):
+    professional_skills = Skill.objects.filter(skill_type='professional')
+    personal_skills = Skill.objects.filter(skill_type='personal')
+    
+    context = {
+        'professional_skills': professional_skills,
+        'personal_skills': personal_skills
+    }
+    return render(request, 'portfolio/portfolio.html', context)

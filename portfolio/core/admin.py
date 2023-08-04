4efcb7720e9539c4333b29dc21234027cef4e437
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-08-05 00:25:54 +0300

    modified:   portfolio/core/__pycache__/admin.cpython-310.pyc

--- portfolio/core/admin.py
+++ portfolio/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Home, About, SkillCategory, Skill, Education, Experience
+from .models import Home, About, SkillCategory, Skill, Education, Experience, Project
 
 admin.site.register(Home)
 
@@ -13,4 +13,6 @@ admin.site.register(Skill)
 admin.site.register(Education)
 
 admin.site.register(Experience)
+
+admin.site.register(Project)
 
--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 
-from .models import Home, About, Skill, SkillCategory, Education, Experience
+from .models import Home, About, Skill, SkillCategory, Education, Experience, Project
 
 def index(request):
     #Home
@@ -30,6 +30,15 @@ def index(request):
     work_description = Experience.objects.all()
     period = Experience.objects.all()
 
+    # Projects
+    project_name = Project.objects.all()
+    github_link = Project.objects.all()
+    live_link = Project.objects.all()
+    project_image = Project.objects.all()
+    project_short_description = Project.objects.all()
+    project_description = Project.objects.all()
+    tools_and_lang = Project.objects.all()
+
 
     return render(request, 'core/index.html', {
         'name': name,
@@ -52,6 +61,14 @@ def index(request):
         'company_location': company_location,
         'work_description': work_description,
         'period': period,
-    })
 
+        'project_name': project_name,
+        'github_link': github_link,
+        'live_link': live_link,
+        'project_image': project_image,
+        'project_short_description': project_short_description,
+        'project_description': project_description,
+        'tools_and_lang': tools_and_lang,
+
+    })
 

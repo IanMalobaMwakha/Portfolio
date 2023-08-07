2ecf15bec3cc3284553499ff3fbd76a5928b30c2
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-08-07 07:08:55 +0300

    modified:   portfolio/core/forms.py

---
+++ portfolio/core/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Project
+
+class ProjectDescriptionForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('project_description')
+        

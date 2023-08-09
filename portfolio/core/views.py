72e1e0f1653fe87f976f17d00c5ecaecaa92952d
Ian Mwakha Maloba <ianmalobamwakha@gmail.com>
2023-08-09 23:22:20 +0300

    modified:   portfolio/core/views.py

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.conf import settings
-from .forms  import ProjectDescriptionForm
 
 from django.shortcuts import get_object_or_404
 from .models import Home, About, Skill, SkillCategory, Education, Experience, ToolsUsed, Project, Contact

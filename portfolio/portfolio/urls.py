2d9b5fe464ae20768b7bc4f02c62c8bf7153db20
Ian Maloba <ianmalobamwakha@gmail.com>
2023-11-11 03:04:04 +0300

    new file:   portfolio/blog/urls.py

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from core.views import index
 from django.contrib import admin
-from core import views
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

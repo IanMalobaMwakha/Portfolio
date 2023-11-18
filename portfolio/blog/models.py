9721029f5f763ad305749df24274a744d4c89e7e
Ian Maloba <ianmalobamwakha@gmail.com>
2023-11-19 00:20:58 +0300

    modified:   portfolio/blog/models.py

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    blog_name = models.CharField(max_length=255)
+    blog_image = models.ImageField(upload_to='blog_images', blank=True, null=True)
+    # date
+    
+    def __str__(self):
+        return self.blog_name
+    
+    class Meta:
+        verbose_name_plural = "BLOGS SECTION"

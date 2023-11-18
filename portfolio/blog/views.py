7f74ada4fee5cb242357f2a87d9d478b0b30224d
Ian Maloba <ianmalobamwakha@gmail.com>
2023-11-18 23:51:18 +0300

    modified:   portfolio/blog/__pycache__/views.cpython-38.pyc

--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from core.models import Home
 
 def index(request):
-    return render(request, "blogs/index.html", {
+    official_image = Home.objects.get()
 
+    return render(request, "blogs/index.html", {
+        "official_image": official_image,
     })
 

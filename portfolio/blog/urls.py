30bccdd2f2affb8635878aa91b2b8e504ec9a626
Ian Maloba <ianmalobamwakha@gmail.com>
2023-11-24 18:06:52 +0300

    modified:   portfolio/blog/views.py

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -7,5 +7,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blog/<int:pk>/', views.blog_body, name='blog_body'),
 ]
 
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from core.models import Home
 from .models import Blog
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
     official_image = Home.objects.get()
@@ -9,5 +11,13 @@ def index(request):
     return render(request, "blogs/index.html", {
         "official_image": official_image,
         'blogs': blogs
+    })
+
+def blog_body(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+
+    return render(request, "blogs/blog_body.html", {
+        'blog': blog,
+        'pk': pk,
     })
 
